ce6178257e97a25bc4b77fa7fe9a8a68df0fa41c
Yan Deng <yanjdeng@gmail.com>
2017-03-03 01:08:54 -0600

    build the my account page.

--- src/test/java/com/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/bookstore/BookstoreApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BookstoreappApplicationTests {
+public class BookstoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
